2eaff8976d663ec00bfbb686735db4aaa76e8fa2
Kittinut Pramhan <knightza94@gmail.com>
2018-06-04 15:48:42 +0700

    add floor recursive functtion

--- problems/problem_02/max-floor.js
+++ problems/problem_02/max-floor.js
@@ -1,7 +1,19 @@
 const maxFloor = input => {
-  // put your code here !!
-    for(i)
-
+    // put your code here !!
+    var floor = 0;
+    const calc = (input, index) => {
+        var result = input - index;
+        if (result === 0) {
+            console.log('floor', floor);
+            return floor + 1;
+        } else if (result < 0) {
+            console.log('floor', floor);
+            return floor;
+        } else if (result > 0) {
+            floor++;
+            return calc(result, index + 1)
+        }
+    }
+    return calc(input, 1)
 };
-
-module.exports = { maxFloor }
+module.exports = {maxFloor}
